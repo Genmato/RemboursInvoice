9cc005963927df219b0a2e3ddf2171895996a1c9
Vladimir <vladimir@kerkhoff.nl>
2013-11-25 14:20:07 +0100

    Initial release

---
+++ app/code/community/Genmato/RemboursInvoice/Model/Method/Invoice.php
@@ -0,0 +1,7 @@
+<?php
+
+class Genmato_RemboursInvoice_Model_Method_Invoice extends Genmato_Payment_Model_Method_Abstract
+{
+
+    protected $_code = 'invoice';
+}
---
+++ app/code/community/Genmato/RemboursInvoice/Model/Method/Rembours.php
@@ -0,0 +1,7 @@
+<?php
+
+class Genmato_RemboursInvoice_Model_Method_Rembours extends Genmato_Payment_Model_Method_Abstract
+{
+
+    protected $_code = 'rembours';
+}
